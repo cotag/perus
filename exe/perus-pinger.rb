195a461edbd8286bbbcb365c9e818e3040f2e0ae
Will Cannings <me@willcannings.com>
2015-06-19 13:30:03 +1000

    Pinger command

---
+++ exe/perus-pinger.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+#require 'perus/pinger'
+require 'optparse'
+
+config_path = '/etc/perus-pinger'
+
+ARGV.options do |opts|
+    opts.banner = "Usage: perus-pinger [options]"
+
+    opts.on('-c', '--config', String, 'Path to config file (default: /etc/perus-pinger)') do |c|
+        config_path = c
+    end
+
+    opts.on('-h', '--help', 'Prints this help') do
+        puts opts
+        exit
+    end
+
+    opts.parse!
+end
+
+pinger = Pinger.new(config_path)
+pinger.ping
