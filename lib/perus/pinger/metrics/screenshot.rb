24c66005029fa3d76dd4528f93b646b058e5c590
Will Cannings <me@willcannings.com>
2015-07-12 21:20:46 +1000

    Change from import to scrot on linux for screenshots. ImageMagick has a bug which was causing hidden windows to be captured in the shot.

--- lib/perus/pinger/metrics/screenshot.rb
+++ lib/perus/pinger/metrics/screenshot.rb
@@ -24,10 +24,19 @@ module Perus::Pinger
                 # backticks to avoid thowing an exception on success
                 `sips -z #{height.to_i * percent} #{width.to_i * percent} #{options.path}`
             else
-                shell("export DISPLAY=:0; import -window root -resize #{options.resize} #{options.path}")
+                shell("export DISPLAY=:0; scrot --silent --thumb #{options.resize.to_i} #{options.path}")
             end
 
-            @screenshot_file = File.new(options.path)
+            # scrot produced two files: path.ext and path-thumb.ext
+            @original_path = options.path
+            name_parts = File.basename(options.path).split('.')
+            name_parts[-2] += '-thumb'
+            @thumbnail_path = File.join(
+                File.dirname(options.path),
+                name_parts.join('.')
+            )
+
+            @screenshot_file = File.new(@thumbnail_path)
             {screenshot: @screenshot_file}
         end
 
@@ -36,7 +45,8 @@ module Perus::Pinger
                 @screenshot_file.close
             end
 
-            File.delete(options.path)
+            File.delete(@original_path)
+            File.delete(@thumbnail_path)
         end
     end
 end
