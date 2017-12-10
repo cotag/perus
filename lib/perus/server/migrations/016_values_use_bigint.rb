04c9f340f39b891d4e1063728bf8df2e8d0c1070
Stephen von Takach <steve@cotag.me>
2017-12-10 14:28:57 +1100

    migrate values table to use bigint

--- lib/perus/server/migrations/016_values_use_bigint.rb
+++ lib/perus/server/migrations/016_values_use_bigint.rb
@@ -1,5 +1,8 @@
 Sequel.migration do
     change do
         change_column :values, :timestamp, :bigint
+        change_column :errors, :timestamp, :bigint
+        change_column :actions, :timestamp, :bigint
+        change_column :active_alerts, :timestamp, :bigint
     end
 end
