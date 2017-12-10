76c2315c44a73b10b9857cf0283cc22299b2982e
Stephen von Takach <steve@cotag.me>
2017-12-10 23:27:25 +1100

    migrate to bigint

--- lib/perus/server/migrations/016_values_use_bigint.rb
+++ lib/perus/server/migrations/016_values_use_bigint.rb
@@ -1,8 +1,8 @@
 Sequel.migration do
     up do
-        execute("ALTER TABLE values CHANGE timestamp timestamp BIGINT")
-        execute("ALTER TABLE errors CHANGE timestamp timestamp BIGINT")
-        execute("ALTER TABLE actions CHANGE timestamp timestamp BIGINT")
-        execute("ALTER TABLE active_alerts CHANGE timestamp timestamp BIGINT")
+        execute("ALTER TABLE values ALTER COLUMN timestamp TYPE bigint")
+        execute("ALTER TABLE errors ALTER COLUMN timestamp TYPE bigint")
+        execute("ALTER TABLE actions ALTER COLUMN timestamp TYPE bigint")
+        execute("ALTER TABLE active_alerts ALTER COLUMN timestamp TYPE bigint")
     end
 end
