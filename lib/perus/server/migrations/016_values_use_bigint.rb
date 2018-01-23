ba78e4b01679c265662d966d7341f95256ddaddf
Stephen von Takach <steve@cotag.me>
2018-01-23 12:57:05 +1100

    Document alter sequence to for restarting id count

--- lib/perus/server/migrations/016_values_use_bigint.rb
+++ lib/perus/server/migrations/016_values_use_bigint.rb
@@ -4,5 +4,13 @@ Sequel.migration do
         execute("ALTER TABLE errors ALTER COLUMN timestamp TYPE bigint")
         execute("ALTER TABLE actions ALTER COLUMN timestamp TYPE bigint")
         execute("ALTER TABLE active_alerts ALTER COLUMN timestamp TYPE bigint")
+
+        execute("ALTER TABLE values ALTER COLUMN id TYPE bigint")
+        execute("ALTER TABLE metrics ALTER COLUMN id TYPE bigint")
+        execute("ALTER TABLE alerts ALTER COLUMN id TYPE bigint")
+        execute("ALTER TABLE active_alerts ALTER COLUMN id TYPE bigint")
+
+        # If a reset needs to be made due to ID limit
+        # "ALTER SEQUENCE values_id_seq RESTART WITH 1;"
     end
 end
