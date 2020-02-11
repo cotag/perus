4735c01931c83dfb48142319c669f99b1da8ae3f
William Le <w.le@acaprojects.com>
2020-02-11 14:25:56 +0800

    chore(script/sql): make systems table use bigint ID

--- lib/perus/server/migrations/016_values_use_bigint.rb
+++ lib/perus/server/migrations/016_values_use_bigint.rb
@@ -9,6 +9,7 @@ Sequel.migration do
         execute("ALTER TABLE metrics ALTER COLUMN id TYPE bigint")
         execute("ALTER TABLE alerts ALTER COLUMN id TYPE bigint")
         execute("ALTER TABLE active_alerts ALTER COLUMN id TYPE bigint")
+        execute("ALTER TABLE systems ALTER COLUMN id TYPE bigint")
 
         # If a reset needs to be made due to ID limit
         # "ALTER SEQUENCE values_id_seq RESTART WITH 1;"
